bb53c3b53232f0cfb5d9799d7a1294d11108b75e
yura-prots <yuraprots@gmail.com>
2024-03-10 20:59:58 +0200

    create useAuth hook

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,15 @@
+import { useSelector } from 'react-redux';
+
+import {
+  selectUser,
+  selectIsLoggedIn,
+  selectIsRefreshing,
+} from 'redux/auth/selector';
+
+export const useAuth = () => {
+  return {
+    user: useSelector(selectUser),
+    isLoggedIn: useSelector(selectIsLoggedIn),
+    isRefreshing: useSelector(selectIsRefreshing),
+  };
+};
