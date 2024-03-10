c4048bd43f5bb3c8353ccdd4aa3958f096356817
yura-prots <yuraprots@gmail.com>
2024-03-10 20:53:09 +0200

    create auth selectors

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -6,6 +6,7 @@ export const authSlice = createSlice({
     user: { name: null, email: null },
     token: null,
     isLoggedIn: false,
+    isRefreshing: false,
     isLoading: false,
     error: null,
   },
