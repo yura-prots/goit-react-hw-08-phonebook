08f0b18b2769f73fffd44ac847848a7c6ca29120
yura-prots <yuraprots@gmail.com>
2024-03-14 20:10:18 +0200

    create PublicRoute

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 
 import Layout from 'components/Layout';
+import PublicRoute from 'components/PublicRoute';
 import { refreshUser } from '../../redux/auth/operations';
 import { useAuth } from 'hooks/useAuth';
 
@@ -26,8 +27,18 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
+          <Route
+            path="/login"
+            element={
+              <PublicRoute component={LoginPage} redirectTo="/contacts" />
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <PublicRoute component={RegisterPage} redirectTo="/contacts" />
+            }
+          />
           <Route path="/contacts" element={<ContactsPage />} />
         </Route>
 
